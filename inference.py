e2502cd4c3f557eba6ace5e02e1b4078d1dad2d1
mekha <mekharemanan@gmail.com>
2025-12-04 19:26:27 +0530

    Updated inference.py and added output.jsonl

--- inference.py
+++ inference.py
@@ -1,11 +1,13 @@
 
 import argparse
+import json
 from PIL import Image
 from transformers import AutoProcessor, AutoModelForCausalLM
 import torch
+import os
 
 # Load merged model
-print("Loading  final pharmacy OCR model...")
+print("Loading final pharmacy OCR model...")
 processor = AutoProcessor.from_pretrained("model_v1", trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained("model_v1", trust_remote_code=True)
 
@@ -25,6 +27,20 @@ def ocr_image(image_path):
     ocr_result = generated_text.split("<OCR>")[-1].split("</OCR>")[0].strip()
     return ocr_result
 
+def save_to_jsonl(image_path, text):
+    output_file = "output.jsonl"   # renamed file
+
+    record = {
+        "filename": os.path.basename(image_path),
+        "extracted_text": text
+    }
+
+    # Append as one-line JSON
+    with open(output_file, "a", encoding="utf-8") as f:
+        f.write(json.dumps(record, ensure_ascii=False) + "\n")
+
+    print(f"\nSaved to: {output_file}")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image", type=str, required=True, help="Path to invoice image")
@@ -32,8 +48,12 @@ if __name__ == "__main__":
 
     print(f"Running OCR on: {args.image}")
     result = ocr_image(args.image)
+
     print("\n" + "="*60)
     print("EXTRACTED TEXT:")
     print("="*60)
     print(result)
     print("="*60)
+
+    # Save result to output.jsonl
+    save_to_jsonl(args.image, result)
